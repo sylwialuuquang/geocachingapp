463807b5ef81c366c38d547301c02cdd2f312b51
Sylwia Luu Quang <sylwialuuquang@gmail.com>
2021-07-05 01:29:08 +0200

    switched to useCallback in Home component

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,13 +1,16 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useCallback} from 'react';
 import {View, Text} from 'react-native';
 import SplashScreen from 'react-native-splash-screen';
 import {StackNavigationProp} from '@react-navigation/stack';
 import {Button} from 'react-native-paper';
 
 import {RootStackParamList} from '../../types';
-import {Screens} from '../../navigation/screen'
+import {Screens} from '../../navigation/screen';
 
-type HomeScreenNavigationProp = StackNavigationProp<RootStackParamList, Screens.HOME>;
+type HomeScreenNavigationProp = StackNavigationProp<
+  RootStackParamList,
+  Screens.HOME
+>;
 
 type Props = {
   navigation: HomeScreenNavigationProp;
@@ -18,21 +21,21 @@ const Home: React.FC<Props> = ({navigation}) => {
     SplashScreen.hide();
   }, []);
 
+  const goToRadiusScreen = useCallback(() => {
+    navigation.navigate(Screens.RADIUS);
+  }, []);
+
+  const goToSunScreen = useCallback(() => {
+    navigation.navigate(Screens.SUN);
+  }, []);
+
   return (
     <View>
-      <Button
-        mode="contained"
-        onPress={() => {
-          navigation.navigate(Screens.RADIUS);
-        }}>
+      <Button mode="contained" onPress={goToRadiusScreen}>
         Search for caches!
       </Button>
       <Text></Text>
-      <Button
-        mode="contained"
-        onPress={() => {
-          navigation.navigate(Screens.SUN);
-        }}>
+      <Button mode="contained" onPress={goToSunScreen}>
         Sunrise and sunset
       </Button>
     </View>
