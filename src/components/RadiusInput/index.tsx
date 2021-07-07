5d8bbb0ec06484fa5bee35a1209ef50bb20db6d9
Sylwia Luu Quang <sylwialuuquang@gmail.com>
2021-07-07 23:26:03 +0200

    RadiusInput screen styled

--- src/components/RadiusInput/index.tsx
+++ src/components/RadiusInput/index.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, Image, StyleSheet} from 'react-native';
 import {StackNavigationProp} from '@react-navigation/stack';
 import {useForm, Controller} from 'react-hook-form';
-import {TextInput, Button} from 'react-native-paper';
+import {TextInput} from 'react-native-paper';
 
 import {RootStackParamList} from '../../types';
 import {Screens} from '../../navigation/screen';
 import SubmitButton from '../SubmitButton';
+import colors from '../../assets/colors/colors';
 
 type FormData = {
   radius: string;
@@ -35,35 +36,67 @@ const RadiusInput: React.FC<Props> = ({navigation}) => {
 
   const radiusRules = {
     required: true,
-    pattern: {value: /[\d]/g, message: 'Digits only'},
+    pattern: {value: /^\d+$/, message: 'Digits only'},
   };
 
   return (
-    <View>
-      <Controller
-        name="radius"
-        defaultValue=""
-        control={control}
-        rules={radiusRules}
-        render={({field: {onChange, onBlur, value}}) => (
-          <TextInput
-            label="Radius"
-            onBlur={onBlur}
-            onChangeText={value => onChange(value)}
-            value={value}
-          />
-        )}
-      />
-      {errors.radius && <Text>{errors.radius.message}</Text>}
-      <Text></Text>
-      <SubmitButton
-        text="Find caches"
-        icon="magnify"
-        mode="contained"
-        onPress={handleSubmit(onSubmit)}
-      />
+    <View style={styles.container}>
+      <Image style={styles.compassImg} source={require('../../assets/images/compass.png')}></Image>
+      <View style={styles.descContainer}>
+        <Text style={styles.descText}>Find caches in your area! Enter radius.</Text>
+      </View>
+      <View style={styles.inputContainer}>
+        <Controller
+          name="radius"
+          defaultValue=""
+          control={control}
+          rules={radiusRules}
+          render={({field: {onChange, onBlur, value}}) => (
+            <TextInput
+              label="Radius"
+              onBlur={onBlur}
+              onChangeText={value => onChange(value)}
+              value={value}
+            />
+          )}
+        />
+        {errors.radius && <Text>{errors.radius.message}</Text>}
+      </View>
+      <View>
+        <SubmitButton
+          text="Find caches"
+          icon="magnify"
+          mode="contained"
+          onPress={handleSubmit(onSubmit)}
+        />
+      </View>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+  },
+  compassImg: {
+    alignSelf: 'center',
+    height: 150,
+    width: 150,
+  },
+  descContainer: {
+    alignSelf: 'center',
+    paddingTop: 30,
+  },
+  descText: {
+    fontFamily: 'Montserrat-Medium',
+    fontSize: 16,
+    color: colors.black,
+    textAlign: 'center'
+  },
+  inputContainer: {
+    paddingVertical: 20,
+  }
+})
 
 export default RadiusInput;
